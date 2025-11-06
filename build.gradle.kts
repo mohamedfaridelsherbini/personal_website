74506527e717c62a08970eb6df3adabc0b0c94b7
Mohamed Elsherbini <m.farid.shawky@gmail.com>
2025-11-06 13:16:36 +0100

    Add ktlint plugin to build.gradle.kts for code quality enforcement

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.21" apply false
     id("io.ktor.plugin") version "2.3.7" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
 }
 
 group = "com.personalwebsite"
